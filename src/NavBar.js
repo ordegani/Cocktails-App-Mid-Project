5c6b21eeabbaf0f4cd40f8ee9afa7e21b3a8c654
ordegani <oradeganii@gmail.com>
2021-04-14 14:18:00 +0300

    adding files

--- src/NavBar.js
+++ src/NavBar.js
@@ -8,7 +8,7 @@ function Navbar() {
             <ul className="navUl">
                 
                 <li><Link className="link" to="/HelloPage">Hello</Link></li>
-                <li><Link className="link" to="/Main">Main</Link></li>
+                <li><Link className="link" to="/">Main</Link></li>
                 <li><Link className="link" to="/Info">Info</Link></li>
                 <li><Link className="link" to="/Random">Surprise</Link></li>
                 {/* </ul>li> <Link to={`/Favourites/`}>Ann's Profile</Link></li> */}

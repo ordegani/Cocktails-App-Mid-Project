c5f5f3eb579ebf73546cb95d16bda571a242be09
ordegani <oradeganii@gmail.com>
2021-04-15 17:49:46 +0300

    change

--- src/NavBar.js
+++ src/NavBar.js
@@ -8,7 +8,7 @@ function Navbar() {
             <ul className="navUl">
                 
                 <li><Link className="link" to="/HelloPage">Hello</Link></li>
-                <li><Link className="link" to="/">Main</Link></li>
+                <li><Link className="link" to="/Main">Main</Link></li>
                 <li><Link className="link" to="/Info">More Info</Link></li>
                 <li><Link className="link" to="/Random">Surprise Me</Link></li>
                 {/* </ul>li> <Link to={`/Favourites/`}>Ann's Profile</Link></li> */}

f3c6bb8ad029da283484c027d9731e7c4f8e11d3
ordegani <oradeganii@gmail.com>
2021-04-15 17:43:17 +0300

    change header

--- src/NavBar.js
+++ src/NavBar.js
@@ -9,8 +9,8 @@ function Navbar() {
                 
                 <li><Link className="link" to="/HelloPage">Hello</Link></li>
                 <li><Link className="link" to="/Main">Main</Link></li>
-                <li><Link className="link" to="/Info">Info</Link></li>
-                <li><Link className="link" to="/Random">Surprise</Link></li>
+                <li><Link className="link" to="/Info">More Info</Link></li>
+                <li><Link className="link" to="/Random">Surprise Me</Link></li>
                 {/* </ul>li> <Link to={`/Favourites/`}>Ann's Profile</Link></li> */}
             </ul>
         </div>

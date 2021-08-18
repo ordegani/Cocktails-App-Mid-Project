b994fd0567c85ab9bce26894562190e1bf0b7ecc
ordegani <oradeganii@gmail.com>
2021-08-19 00:17:40 +0300

    changes

--- src/HelloPage.js
+++ src/HelloPage.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-
-const HelloPage = ()=>{
-    return(
-        <div className="helloContainer">
-        <div className="helloMessage">
-            Welcome!<br />
-            <br />
-            Here you will find exciting new cocktails you can make by yourself at home. <br />
-            The app includes many many cocktails - and lets you know the amount of calories each includes.<br />&#127865;<br />
-            Search an ingredient (like a spice or fruit) <br/> View results<br />
-            Save cocktails you want to make by clicking on the 'save' button. <br />&#127865;<br />
-            Please Check the Info section for tips on how to drink and diet wizely.<br />
-            If you don't like choosing we can randomize a surprise for you - Try navigating to 'Surprise' and see what happens.<br/> 
-            Enjoy!<br />&#127865;
-        </div>
-        </div>
-    )
-
-
-}
+const HelloPage = () => {
+  return (
+    <div className="helloContainer">
+      <div className="helloMessage">
+        Welcome!
+        <br />
+        <br />
+        Here you will find exciting new cocktails you can make by yourself at
+        home. <br />
+        The app includes many many cocktails - and lets you know the amount of
+        calories each includes.
+        <br />
+        &#127865;
+        <br />
+        Search an ingredient (like a spice or fruit) <br /> View results
+        <br />
+        Save cocktails you want to make by clicking on the 'save' button. <br />
+        &#127865;
+        <br />
+        Please Check the Info section for tips on how to drink and diet wizely.
+        <br />
+        If you don't like choosing we can randomize a surprise for you - Try
+        navigating to 'Surprise' and see what happens.
+        <br />
+        Enjoy!
+        <br />
+        &#127865;
+      </div>
+    </div>
+  );
+};
 
 export default HelloPage;
